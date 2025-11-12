88c86eb356e3e00347afb2392195ef719b723afe
crist <cr1st4ever@outlook.com>
2025-11-12 17:44:55 +0800

    添加了一个接口，用来根据类型查询结点数据 删除了枚举，考虑使用前端处理

--- repository/message_repo.go
+++ repository/message_repo.go
@@ -5,27 +5,48 @@ import (
 	"EmqxBackEnd/models"
 )
 
-type MessageType string
-
-const (
-	Register    MessageType = "0"
-	Confirm     MessageType = "1"
-	Temperature MessageType = "2"
-)
-
-var messageTypeMap = map[MessageType]string{
-	Register:    "注册",
-	Confirm:     "确认",
-	Temperature: "温度",
-}
+// 在前端处理
+//type MessageType string
+//
+//const (
+//	Register    MessageType = "0"
+//	Confirm     MessageType = "1"
+//	Temperature MessageType = "2"
+//	Humidity    MessageType = "3"
+//	MQ6         MessageType = "4"
+//)
+//
+//var messageTypeMap = map[MessageType]string{
+//	Register:    "注册",
+//	Confirm:     "确认",
+//	Temperature: "温度",
+//	Humidity:    "湿度",
+//	MQ6:         "天然气",
+//}
 
 func SaveMessage(msg *models.EmpxMessage) error {
 	// 注意 postgres sql 插入时，表要标明在哪个域（public）内
 	query := `INSERT INTO public.packets (node_id, type, message, received_at) VALUES ($1, $2, $3, $4)`
-	realType := "未知"
-	if desc, exists := messageTypeMap[MessageType(msg.Type)]; exists {
-		realType = desc
-	}
-	_, err := database.DB.Exec(query, msg.NodeID, realType, msg.Value, msg.TS)
+	_, err := database.DB.Exec(query, msg.NodeID, msg.Type, msg.Value, msg.TS)
 	return err
+}
+
+// 读取数据库
+func GetMessages(msgType string) ([]models.EmpxMessage, error) {
+	query := `SELECT node_id, type, message, received_at FROM public.packets WHERE type = $1`
+	rows, err := database.DB.Query(query, msgType)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var messages []models.EmpxMessage
+	for rows.Next() {
+		var msg models.EmpxMessage
+		err := rows.Scan(&msg.NodeID, &msg.Type, &msg.Value, &msg.TS)
+		if err != nil {
+			return nil, err
+		}
+		messages = append(messages, msg)
+	}
+	return messages, nil
 }
