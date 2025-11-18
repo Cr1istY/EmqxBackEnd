8760e39e9959c0224e2c17535296d874071bd447
crist <cr1st4ever@outlook.com>
2025-11-18 17:25:39 +0800

    feat(node): 节点修改接口
    - 与节点注册接口通用
    - 当检测到节点存在时，即更新
    - 只有不存在时，才在表中新建

--- repository/node_repo.go
+++ repository/node_repo.go
@@ -17,9 +17,26 @@ func GetUserIdByNodeId(nodeId int) (int, error) {
 	return userId, nil
 }
 
-// 绑定节点
+// SaveNode 绑定节点
 func SaveNode(nodeId, userId int) error {
 	query := `insert into public.node(id, user_id) values($1, $2)`
 	_, err := database.DB.Exec(query, nodeId, userId)
 	return err
+}
+
+func UpdateNode(nodeId, userId int) error {
+	query := `update public.node set user_id=$1 where id=$2`
+	_, err := database.DB.Exec(query, userId, nodeId)
+	return err
+}
+
+func CheckNode(nodeId int) (bool, error) {
+	query := `select exists(select 1 from public.node where id=$1)`
+	var exists bool
+	err := database.DB.QueryRow(query, nodeId).Scan(&exists)
+	if err != nil {
+		log.Println("CheckNode error:", err)
+		return false, err
+	}
+	return exists, nil
 }
--- service/node_service.go
+++ service/node_service.go
@@ -6,5 +6,12 @@ import (
 )
 
 func SaveNode(node *models.Node) error {
+	isHave, err := repository.CheckNode(node.ID)
+	if err != nil {
+		return err
+	}
+	if isHave {
+		return repository.UpdateNode(node.ID, node.UserId)
+	}
 	return repository.SaveNode(node.ID, node.UserId)
 }
